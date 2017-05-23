0cc298d97bfccc4b48a99759718a4b8c626f7ae5
HenryCo <hd2files@gmail.com>
2017-05-24 00:29:22 +0200

    Release

--- SQLightningLib/src/test/java/net/henryco/sqlightning/promo/ExampleRepository.java
+++ SQLightningLib/src/test/java/net/henryco/sqlightning/promo/ExampleRepository.java
@@ -1,5 +1,11 @@
 package net.henryco.sqlightning.promo;
 
+import android.database.Cursor;
+
+import net.henryco.sqlightning.reflect.annotations.repository.Alias;
+import net.henryco.sqlightning.reflect.annotations.repository.Implementation;
+import net.henryco.sqlightning.reflect.annotations.repository.Query;
+import net.henryco.sqlightning.reflect.annotations.repository.Read;
 import net.henryco.sqlightning.reflect.annotations.repository.Repository;
 import net.henryco.sqlightning.reflect.repository.LightningRepository;
 
@@ -8,5 +14,15 @@ import net.henryco.sqlightning.reflect.repository.LightningRepository;
  */
 @Repository("user_table")
 public interface ExampleRepository extends LightningRepository<Long, SomeUser> {
+
+
+	@Read // optional annotation, because SELECT case enables Read mode automatically
+	@Query("SELECT * FROM user_table WHERE id = {uid} AND first_name = {name}")
+	Cursor someMethodOne(@Alias("uid") long id, @Alias("name") String name);
+
+
+	@Implementation(MethodsImp.class)
+	boolean someMethodTwo(long id, ExampleRepository self);
+
 
 }
---
+++ SQLightningLib/src/test/java/net/henryco/sqlightning/promo/MethodsImp.java
@@ -0,0 +1,14 @@
+package net.henryco.sqlightning.promo;
+
+/**
+ * Created by HenryCo on 24/05/17.
+ */
+
+public class MethodsImp {
+
+
+	boolean someMethodTwo(long id, ExampleRepository self) {
+		// some method
+		return true;
+	}
+}
